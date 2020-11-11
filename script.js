2ce91a7fe9ee2a9159d0d79c3fbccf887d394e3f
nagarjuna0629 <nagarjuna0629@gmail.com>
2020-11-11 21:59:06 +0530

    added multiple inputs to linear gradient

--- script.js
+++ script.js
@@ -1,16 +1,74 @@
-var css = document.querySelector("h3");
+var button = document.getElementById("enter");
+var clear=document.getElementById("clear");
+var input = document.getElementById("userinput");
 var deg= document.getElementById("deg");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var colors=document.querySelector("#DIV");
+var css = document.querySelector("h3");
+var array=[];
 
-function setGradient()
-{
-	body.style.background = "linear-gradient("+deg.value+"deg,"+ color1.value +", "+ color2.value +")";
-	console.log(deg.value);
-	css.textContent = body.style.background + ";";
+function inputLength() {
+	return input.value.length;
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+function setGradient(){
+document.body.style.background="linear-gradient("+deg.value+"deg,"+ array +")";
+css.textContent = document.body.style.background + ";";
+}
+
+
+function createListElement(){
+var n=input.value;
+console.log(n);
+var i;
+var apnd = array.length;
+for(i=0;i<n;i++)
+{
+var x= document.createElement("INPUT");
+x.setAttribute("id",apnd+i);
+x.setAttribute("type","color");
+x.setAttribute("value","#00ff00");
+document.getElementById("DIV").appendChild(x);
+x.addEventListener("input",setGradient,false);
+array[apnd+i]=x.value;
+}
+input.value = "";
+}
+
+function removeListAfterClick()
+{
+var d=document.getElementById("DIV");
+if (d.hasChildNodes()) {
+  d.removeChild(d.childNodes[0]);
+  array.shift();
+}
+setGradient();
+}
+
+function colorValueChange(e){
+if(e.target !== e.currentTarget)
+{
+var elid = e.target.id;
+array[elid]=e.target.value;
+}
+e.stopPropagation();
+}
+
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+clear.addEventListener("click", removeListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
 deg.addEventListener("input", setGradient);
+colors.addEventListener("input",colorValueChange,false);
+
